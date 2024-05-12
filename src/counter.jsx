dc1f31d3e73eef4593f238b6bd8655cb8da41147
Abhinav hazarika <abhinavhazarika27@gmail.com>
2024-05-12 20:59:48 +0530

    xyz

---
+++ src/counter.jsx
@@ -0,0 +1,32 @@
+
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
